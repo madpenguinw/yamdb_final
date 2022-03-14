2faefe96c160d63be3e697dafc3df8db5e24e32f
Mikhail Sokolov <lmikhailsokolovl@gmail.com>
2022-03-14 18:25:31 +0300

    Hope it works

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# отличие для пуша
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs')
