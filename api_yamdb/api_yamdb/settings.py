8c012e9cdbfed7f392fc4aec305e694131b71089
Mikhail Sokolov <lmikhailsokolovl@gmail.com>
2022-03-21 02:09:48 +0300

    trying to fix static and redoc

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -104,7 +104,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'original_static/'),)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'original_static/'),)
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
